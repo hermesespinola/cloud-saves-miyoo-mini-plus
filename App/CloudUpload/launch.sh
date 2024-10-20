6135732390869c4e0344a36d02a92adaf54f58a8
Hermes Espínola González <hermes.espinola@gmail.com>
2024-10-19 18:37:22 -0700

    Update launch.sh

--- App/CloudUpload/launch.sh
+++ App/CloudUpload/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo $0 $*
-LD_PRELOAD=/mnt/SDCARD/miyoo/lib/libpadsp.so /mnt/SDCARD/.tmp_update/bin/infoPanel -t "Uploading saves" -m "Your saves are being uploaded to the cloud!" --auto
+LD_PRELOAD=/mnt/SDCARD/miyoo/lib/libpadsp.so /mnt/SDCARD/.tmp_update/bin/infoPanel -t "Uploading saves" -m "Your saves are being uploaded to the cloud!" --auto &
 
 cd $(dirname "$0")
 
-/mnt/SDCARD/rclone copy -P -L --no-check-certificate /mnt/SDCARD/Saves/CurrentProfile/ cloud:Onion/saves/retroarch/ 
+/mnt/SDCARD/rclone sync -P -L --no-check-certificate --combined .rclone-sync-diff.diff /mnt/SDCARD/Saves/CurrentProfile/ cloud:Onion/saves/retroarch/
